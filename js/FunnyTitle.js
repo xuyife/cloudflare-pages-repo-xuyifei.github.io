106b23d255be42c309c94a7bfdc4fb5b32f355f9
xuyife <18916947008@163.com>
2024-03-22 18:42:54 +0800

    Site updated: 2024-03-22 18:42:54

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,9 +1,8 @@
-<script type="text/javascript">
 // 浏览器搞笑标题
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
-	if (location.href != "https://www.xxxzhy.com/"){//主页不变
+	if (location.href != "https://xuyifei.kaiyuan.fund/"){//主页不变
 		if (document.hidden) {
 			//$('[rel="icon"]').attr('href', "/funny.ico");
 			document.title = '╭(°A°`)╮正在删除你的电脑！';
@@ -17,4 +16,3 @@ document.addEventListener('visibilitychange', function () {
 		}
 	}
 });
-</script>
