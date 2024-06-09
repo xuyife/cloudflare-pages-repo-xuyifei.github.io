d05ec450e579840fb335edf537a7d844872994e0
xuyife <18916947008@163.com>
2024-06-09 17:43:41 +0800

    Site updated: 2024-06-09 17:43:40

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,18 +1,1 @@
-// 浏览器搞笑标题
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-	if (location.href != "https://xuyifei.kaiyuan.fund/"){//主页不变
-		if (document.hidden) {
-			//$('[rel="icon"]').attr('href', "/funny.ico");
-			document.title = '╭(°A°`)╮正在删除你的电脑！';
-			clearTimeout(titleTime);
-		}else {
-			//$('[rel="icon"]').attr('href', "/favicon.ico");
-			document.title = '(ฅ>ω<*ฅ) 噫假的啦~' + OriginTitle;
-			titleTime = setTimeout(function () {
-				document.title = OriginTitle;
-			}, 2000);
-		}
-	}
-});
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){"https://xuyifei.kaiyuan.fund/"!=location.href&&(document.hidden?(document.title="╭(°A°`)╮正在删除你的电脑！",clearTimeout(titleTime)):(document.title="(ฅ>ω<*ฅ) 噫假的啦~"+OriginTitle,titleTime=setTimeout(function(){document.title=OriginTitle},2e3)))});
